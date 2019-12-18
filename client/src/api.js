5dc9c790c01e47a113e0ed3ad58e60bca0eafe61
Kingsley Raspe <kingsley.raspe@googlemail.com>
2019-12-17 18:03:37 -0600

    debugging

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: `http://localhost:8080/`,
+  // baseURL: `http://localhost:8080/`,
   headers: {
     Authorization: 'Bearer {token}',
     Accept: 'application/json',
