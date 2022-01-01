ba529b029cc9a3cc4d75bc9371b6ced02a242697
Kingsley Raspe <kingsley.raspe@googlemail.com>
2022-01-01 14:19:01 -0600

    adjusted timeout

--- api/app.js
+++ api/app.js
@@ -141,9 +141,11 @@ if (process.env.NODE_ENV === 'development') {
 /**
  * Start Express server.
  */
-app.listen(app.get('port'), () => {
+const server = app.listen(app.get('port'), () => {
   console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
   console.log('  Press CTRL-C to stop\n');
 });
+
+server.setTimeout(500000);
 
 module.exports = app;
