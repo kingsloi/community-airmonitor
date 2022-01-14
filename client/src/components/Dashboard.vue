fe4f1e65efa76c60635079ee2ae7a0c7007c2767
Kingsley Raspe <kingsley.raspe@googlemail.com>
2022-01-14 17:25:56 -0600

    map zoom level

--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -1543,7 +1543,8 @@ export default {
             L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
                 subdomains: 'abcd',
-                maxZoom: 18
+                maxZoom: 20,
+                maxNativeZoom: 18
             }).addTo(map);
 
             const LeafIcon = L.Icon.extend({
